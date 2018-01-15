ed96b7d907f993950db74b0001518e00680d30f8
Thomas Kint <thomaskint.pro@gmail.com>
2018-01-15 04:15:24 +0100

    Envoie d'un fichier sous forme de byte[] et lecture

--- src/main/java/com/templux/client/TempluxClientEndpoint.java
+++ src/main/java/com/templux/client/TempluxClientEndpoint.java
@@ -5,6 +5,8 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import java.io.*;
+import java.nio.file.Files;
+import java.util.Base64;
 
 /**
  * Created by tkint on 14/01/2018.
@@ -12,7 +14,7 @@ import java.io.*;
 @ClientEndpoint
 public class TempluxClientEndpoint {
 
-	private static final String TEST = "D:\\tkint\\Music\\ACDC\\TNT.mp3";
+	private static final String TEST = "D:\\Thomas\\Music\\ACDC\\TNT.mp3";
 
 	@OnOpen
 	public void onOpen(Session session) {
@@ -27,6 +29,7 @@ public class TempluxClientEndpoint {
 	@OnMessage
 	public byte[] onMessage(String message, Session session) {
 		System.out.println("----- Client -----");
+		System.out.println(message);
 		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			System.out.println("Choisir un fichier à envoyer");
@@ -39,21 +42,12 @@ public class TempluxClientEndpoint {
 
 	private byte[] getByteArrayFromFile(String path) {
 		File file = new File(path);
-
+		byte[] bytes = null;
 		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-
-			}
-			return byteArrayOutputStream.toByteArray();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			bytes = Files.readAllBytes(file.toPath());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return bytes;
 	}
 }
--- src/main/java/com/templux/server/TempluxServerEndpoint.java
+++ src/main/java/com/templux/server/TempluxServerEndpoint.java
@@ -1,15 +1,27 @@
 package com.templux.server;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import sun.audio.AudioData;
+import sun.audio.AudioDataStream;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+import javax.sound.sampled.*;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.*;
+import java.nio.file.Files;
 
 /**
  * Created by tkint on 14/01/2018.
  */
 @ServerEndpoint(value = "/server")
 public class TempluxServerEndpoint {
+
+	private final String TEMP_DIR = "D:/tmp";
 
 	@OnOpen
 	public void onOpen(Session session) {
@@ -23,11 +35,33 @@ public class TempluxServerEndpoint {
 	}
 
 	@OnMessage
-	public String onMessage(byte[] file, Session session) {
+	public String onMessage(byte[] bytes, Session session) {
 		System.out.println("---- Serveur -----");
-		if (file != null) {
+		if (bytes != null) {
+			playMp3(bytes);
 			return "Fichier reçu";
 		}
 		return "Erreur";
+	}
+
+	private void playMp3(byte[] bytes) {
+		File directory = new File(TEMP_DIR);
+		try {
+			Files.createDirectories(directory.toPath());
+			File file = Files.createTempFile(directory.toPath(), "templux-", ".mp3").toFile();
+
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			fileOutputStream.write(bytes);
+			fileOutputStream.close();
+
+			new javafx.embed.swing.JFXPanel();
+
+			Media media = new Media(file.toURI().toString());
+			MediaPlayer mediaPlayer = new MediaPlayer(media);
+			mediaPlayer.play();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
